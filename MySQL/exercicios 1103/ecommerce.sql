d4c6cc2d867a0f2f7a44ef843b4511a5783089cb
Andressa Santos <andressasantosp0@gmail.com>
2021-03-11 12:48:59 -0300

    atividade 2

---
+++ MySQL/exercicios 1103/ecommerce.sql
@@ -0,0 +1,19 @@
+use ecommerce;
+
+create table estoque ( 
+	id int NOT NULL AUTO_INCREMENT,
+	nome_produto varchar(50) NOT NULL,
+    codigo_produto smallint,
+    preco_produto decimal(8,2),
+    peso_produto decimal(6,2),
+    fabricante_produto varchar(40),
+    primary key(id)
+)default charset = utf8mb4;
+
+select * from estoque where preco_produto > 500;
+select * from estoque where preco_produto < 500;
+
+update estoque set preco_produto = 349.99
+where id = 3;
+
+select * from estoque
